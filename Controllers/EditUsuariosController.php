93798ee8dcacd8e8a1fca273232cbbc02b3b888f
BobadillaLeonRodrigo <al222110815@gmail.com>
2023-08-23 10:39:41 -0600

    Actualizacion funcional, comentario para tipo de variable

--- Controllers/EditUsuariosController.php
+++ Controllers/EditUsuariosController.php
@@ -13,16 +13,24 @@ if (!empty($_POST["btnmodificar"])) {
         $emailu = $_POST["emailu"];
         $id_departamento = $_POST["id_departamento"];
         // se realiza la consulta para agregar que campos se van a actualizar del formulario
-        $sql = $conexion->query("UPDATE usuarios SET nombreu='$nombreu',
-                                    appu='$appu',
-                                    apmu='$apmu',
-                                    emailu='$emailu',
-                                    id_departamento=$id_departamento WHERE id_usuario=$id ");
+        /*
+        En el contexto de bind_param, los caracteres utilizados en la cadena representan los tipos de datos esperados.
+        Cada caracter se relaciona con una variable que será vinculada a la consulta.
+        Aquí está la correspondencia de caracteres y tipos de datos:
+            "s": Tipo de cadena (string)
+            "i": Tipo de entero
+        */
+        $stmt = $conexion->prepare("UPDATE usuarios SET nombreu=?, appu=?, apmu=?, emailu=?, id_departamento=? WHERE id_usuario=?");
+        $stmt->bind_param("ssssii", $nombreu, $appu, $apmu, $emailu, $id_departamento, $id);
+        $result = $stmt->execute();
+        $stmt->close();
         //si se registro correctamente redireccionara a la pagina principal
-        if ($sql == 1) {
+        if ($result) {
             // si se modifico correctamente
             // Redirige a otra página
             echo '<div class="alert alert-success text-center"><i class="bi bi-person-add"></i> Usuario Modificado Correctamente</div>';
+            echo '<script>setTimeout(function() { window.location.href = "http://localhost/SGMEC-Conalep/index.php"; }, 1000);</script>';
+            //echo '<script>window.location.href = "http://localhost/SGMEC-Conalep/index.php";</script>';
         } else {
             //si al modificar marca error
             echo '<div class="alert alert-danger text-center">Error al Modificar al Usuario <i class="fa-solid fa-triangle-exclamation fa-xl" style="color: red"></i></div>';
